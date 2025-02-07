eea77814b3441021a5426e2aa078f3b7f3ea2662
mac.lee <162660808+hohohoho1223@users.noreply.github.com>
2025-02-07 23:12:08 +0900

    Update 2025.02.05 numpy_&_pandas.py

--- [파이썬 실습]/2025.02.05 numpy_&_pandas.py
+++ [파이썬 실습]/2025.02.05 numpy_&_pandas.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1s9YAcdh7DsAm3Padz6n9gSalUP08SEKt
 """
 
+```Python
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -537,3 +538,4 @@ print(y)
 y = [x[0],x[2]]
 print(y)
 
+```
